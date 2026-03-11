596ff9a8a133ca97367e8d07681d6040e5d48991
Vaishnavi Tyagi <elsathequeen580@gmail.com>
2026-03-11 20:18:15 +0530

    Add large pickle files using LFS

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-# Load files
-# Ensure movies.pkl contains the 'title' and 'img' columns
+
 book = pickle.load(open('movies.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 vectors = pickle.load(open('vectors.pkl', 'rb'))
@@ -13,21 +12,20 @@ bk=pickle.load(open('bk.pkl', 'rb'))
 @st.cache_data
 def get_poster(title):
     try:
-        # Find the URL in the 'img' column for the matching title
-        # We use .iloc[0] to get the actual string value
+
         url = bk[bk['title'] == title]['img'].values[0]
 
-        # Streamlit Cloud requires HTTPS
+
         if isinstance(url, str):
             return url.replace("http://", "https://")
     except Exception:
         pass
-    # Fallback placeholder if URL is missing or broken
+
     return "https://placehold.co/128x195?text=No+Cover"
 
 
 def recom(bk_title):
-    # Find index of the book
+
     indx = book[book['title'].str.contains(bk_title, case=False)].index[0]
     distance, indices = model.kneighbors(vectors[indx], n_neighbors=6)
 
@@ -37,13 +35,13 @@ def recom(bk_title):
     for i in indices[0][1:]:
         title = book.iloc[i].title
         recomnd_book.append(title)
-        # Fetch the cover URL directly from our function
+
         book_cover.append(get_poster(title))
 
     return recomnd_book, book_cover
 
 
-# UI Code
+
 st.title('Book Recommender System')
 
 book_list = book['title'].values
@@ -55,6 +53,6 @@ if st.button("Recommend"):
     cols = st.columns(5)
     for i in range(5):
         with cols[i]:
-            # Use the covers list returned by recom()
+
             st.image(covers[i])
             st.text(recommendation[i])
